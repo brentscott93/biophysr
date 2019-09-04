e98e17edada66239585deb0a942d0ee8512e2dde
brentscott93 <brentscott93@gmail.com>
2019-09-04 11:42:31 -0400

    update cardiac

--- R/analyze_cardiac_motility.R
+++ R/analyze_cardiac_motility.R
@@ -2,7 +2,7 @@
 #' Analyze cardiac motility
 #'
 #' Works the same as analyze_motility() but with Distance >=1 inclusion criteria
-#'get
+#'
 #' @export
 #'
 #' @param data
@@ -12,23 +12,31 @@
 #' @examples
 analyze_cardiac_motility <- function(data){
 
+  #calculates average velocity
   velocity_average <- data %>%
-    dplyr::filter(Distance >= 4) %>%
+    dplyr::filter(Distance >= 1) %>%
     summarize(average_velocity = mean(AvgSpeed))
 
+  #gets number of moving filaments
   moving_filaments <- data %>%
     dplyr::filter(Distance >= 1) %>%
     count()
 
+  #renaming output of count()
   names(moving_filaments)[names(moving_filaments) == 'n'] <- 'moving_filaments'
 
+  #counts total number of filaments
   all_filament <- data %>%
     count()
 
+  #renaming output of count()
   names(all_filament)[names(all_filament) == 'n'] <- 'total_filaments'
 
-  calculate_percent_moving <- left_join(moving_filaments, all_filament) %>%
+  #calculate percent moving
+  calculate_percent_moving <- right_join(moving_filaments, all_filament) %>%
     mutate(percent_moving = moving_filaments/total_filaments)
 
-  left_join(velocity_average, calculate_percent_moving)
+  #combine the average velocty and percent moving objects. Replacing NAs with 0s.
+  right_join(velocity_average, calculate_percent_moving) %>%
+    replace_na(list(average_velocity = 0, moving_filaments = 0, percent_moving = 0))
 }
