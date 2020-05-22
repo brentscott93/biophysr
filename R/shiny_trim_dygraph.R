1fd903c0b8ef07103c70d8b6a311ad9c7a09e1b3
brentscott93 <brentscott93@gmail.com>
2020-05-22 12:02:17 -0400

    fix trimming data

--- R/shiny_trim_dygraph.R
+++ R/shiny_trim_dygraph.R
@@ -8,11 +8,15 @@
 #' @examples
 shiny_trim_dygraph <- function(trap_selected_obs, trap_grouped_file, input_dygraph_clean_shave_date_window_1, input_dygraph_clean_shave_date_window_2){
 
+#trap_grouped_file <- read.csv("/Users/brentscott/Desktop/myoV-WT_2ndConrtol _obs-01/grouped.csv")
 withProgress(message = "Trimming Data", min= 0, max = 1, value = 0.3, {
-  from <- input_dygraph_clean_shave_date_window_1 * 5000
-    to <- input_dygraph_clean_shave_date_window_2 * 5000
+  #from <- 49.2836 *5000
+  #to <- 60 *5000
+  from <- as.integer(input_dygraph_clean_shave_date_window_1 * 5000)
+    to <- as.integer(input_dygraph_clean_shave_date_window_2 * 5000)
 
   trimmed <- trap_grouped_file[-c(from:to),]
+
 setProgress("Writing new 'grouped' file", value = 0.8)
   write_csv(trimmed, path = paste0(trap_selected_obs, "/grouped.csv"), append = FALSE)
 
